49efb1cd65fa5aa37b49c70a83018b031b5d0abe
cmschmidt <sch21103@byui.edu>
2022-09-26 10:17:07 -0600

    moved javascript to javascript file, added googlefont

--- lesson2/scripts/javascript.js
+++ lesson2/scripts/javascript.js
@@ -1,4 +1,4 @@
-let date = new Date();
+{let date = new Date();
 
 document.getElementById("year").innerHTML = date.getFullYear()
 
@@ -7,4 +7,4 @@ document.getElementById("lastModified").innerHTML = document.lastModified
 function toggleMenu() {
     document.getElementById("primNav").classList.toggle("hide")
 }
-
+}
