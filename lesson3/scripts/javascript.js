1fb6091c9f92f929988f2dff40f19380d0fa85af
cmschmidt <sch21103@byui.edu>
2022-10-08 21:18:35 -0600

    initial commit

---
+++ lesson3/scripts/javascript.js
@@ -0,0 +1,10 @@
+{let date = new Date();
+
+    document.getElementById("year").innerHTML = date.getFullYear()
+    
+    document.getElementById("last-modified").innerHTML = document.lastModified
+    
+    function toggleMenu() {
+        document.getElementById("primNav").classList.toggle("hide")
+    }
+    }
