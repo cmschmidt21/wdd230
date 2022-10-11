90ada19dd9162d57fce8e0f122b6a8b4f6d82f3c
cmschmidt <sch21103@byui.edu>
2022-10-11 17:41:55 -0600

    initial commit

---
+++ chamber/scripts/javascript.js
@@ -0,0 +1,21 @@
+{let date = new Date();
+
+    document.getElementById("year").innerHTML = date.getFullYear()
+    
+    document.getElementById("lastModified").innerHTML = document.lastModified
+    
+    function toggleMenu() {
+        document.getElementById("primNav").classList.toggle("hide")
+    }
+    }
+    
+function toggleMenu() {
+    var temp = document.getElementsByClassName("toggler")[0]
+    temp.classList.toggle("hide")
+    if (temp.classList[1]=="hide") {
+        document.querySelector(".toggler > a").innerHTML = "&#9776; Menu"
+    }
+    else {
+        document.querySelector(".toggler > a").innerHTML = "&#10006; Close"
+    }
+}
