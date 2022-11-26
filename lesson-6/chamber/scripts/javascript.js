cce649e8fb5d8522dda3dc68ac77b14117d90ec9
cmschmidt <sch21103@byui.edu>
2022-11-26 14:32:04 -0700

    initial commit assignment

---
+++ lesson-6/chamber/scripts/javascript.js
@@ -0,0 +1,25 @@
+let date = new Date();
+
+    document.getElementById("year").innerHTML = date.getFullYear()
+    
+    document.getElementById("lastModified").innerHTML = document.lastModified
+    
+    function toggleMenu() {
+        document.getElementById("primNav").classList.toggle("hide")
+    }
+
+const datefield = document.querySelector(".date");
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+console.log(fulldateUK);
+datefield.innerHTML = 
+`<em>${fulldateUK}</em>`;
+
+
+    const hamburger = document.querySelector(".ham");
+    const mobileNav = document.querySelector(".mobile-nav");
+    hamburger.addEventListener("click",() => {mobileNav.classList.toggle("navigation")}, false);
+    window.onresize = () => {if (window.innerWidth >= 600)mobileNav.classList.remove("navigation")};
